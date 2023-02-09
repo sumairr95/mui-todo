c576f94581ff15daf705797b4e15422bd9578e0c
sumair rana <sumairr95@gmail.com>
2023-02-09 10:17:12 -0500

    mui-todos

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,4 +26,20 @@ const Navbar = () => {
         setAnchorElNav(null);
     };
 };
+return (
+    <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <AdbIcon sx={{ dsiplay: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>My todos</Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+                        <MenuIcon />
+                    </IconButton>
+
+                </Box>
+            </Toolbar>
+        </Container>
+    </AppBar>
+)
 export default Navbar;
