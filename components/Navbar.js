b8647d62c05e416b426a8821280dca57812b0da7
sumair rana <sumairr95@gmail.com>
2023-02-09 10:46:10 -0500

    mui-todo

--- components/Navbar.js
+++ components/Navbar.js
@@ -36,10 +36,17 @@ return (
                     <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
                         <MenuIcon />
                     </IconButton>
-
+                    <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' }, }}>
+                        {pages.map((page) => (<MenuItem key={page} onClick={handleCloseNavMenu}><Typography textAlign="center">{page}</Typography></MenuItem>))}
+                    </Menu>
+                </Box>
+                <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                <Typography variant="h5" noWrap component="a" href="" sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>My todos</Typography>
+                <Box sx={{ flexGrow: 1, dsiplay: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (<Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>{page}</Button>))}
                 </Box>
             </Toolbar>
         </Container>
     </AppBar>
-)
+);
 export default Navbar;
