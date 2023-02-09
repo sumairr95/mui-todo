ea73888e0a736b349f5c4d224d08ef48e367ea54
sumair rana <sumairr95@gmail.com>
2023-02-09 11:33:24 -0500

    mui-todo

---
+++ components/Card.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Card from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export deafault function
+    TodosCard({ todo }) {
+    return (
+        <Card>
+            <CardActionArea>
+                <CardContent>
+                    <Typography
+                        gutterBottom variant="h5" component="div">
+                        {todo['title']}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">{todo['description']}</Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
